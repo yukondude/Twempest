c5e6624ed7ef66cd9137c79426e5205a5d323769
Dave Rogers <thedude@yukondude.com>
2017-02-10 10:13:24 -0800

    Choose the most likely valid configuration path.

--- twempest/__main__.py
+++ twempest/__main__.py
@@ -11,8 +11,28 @@ import click
 import tweepy
 
 
+CONFIG_FILE_NAME = "twempest.conf"
+DEFAULT_CONFIG_PATH = "~/.twempest/"
+
+
+def choose_config_path(cli_config_path):
+    """ Choose the most likely configuration path from, in order: the given path, the default path, and the path of this module. To be
+        valid, the config path must contain a twempest.conf file.
+    """
+    here = os.path.dirname(os.path.realpath(__file__))
+    abs_cli_config_path = os.path.abspath(os.path.expanduser(cli_config_path))
+
+    for possible_path in (abs_cli_config_path, DEFAULT_CONFIG_PATH, here):
+        if os.path.isfile(os.path.join(possible_path, CONFIG_FILE_NAME)):
+            return possible_path
+
+    raise click.ClickException("Could not find twempest.conf file in config path '{}' or alternate locations.".format(abs_cli_config_path))
+
+
 # noinspection PyUnusedLocal
 def show_version(ctx, param, value):
+    """ Display the version message.
+    """
     if not value or ctx.resilient_parsing:
         return  # pragma: no cover
     from twempest import __version__
@@ -22,23 +42,25 @@ def show_version(ctx, param, value):
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option("--config-path", "-c", default=DEFAULT_CONFIG_PATH, show_default=True,
+              help="Twempest configuration directory path. The twempest.conf file must exist in this location.")
+@click.option("--include-retweets", "-r", is_flag=True, default=False, help="Include retweets in the list of retrieved tweets.")
 @click.option("--version", "-V", is_flag=True, callback=show_version, expose_value=False, is_eager=True, help="Show version and exit.")
-def twempest():
+def twempest(config_path, include_retweets):
     """ Download a sequence of recent Twitter tweets and convert these, via template, to text format.
     """
-    here = os.path.dirname(os.path.realpath(__file__))
-
     config = configparser.ConfigParser()
-    config.read(os.path.join(here, "twempest.conf"))
+    config_path = choose_config_path(config_path)
+    config.read(os.path.join(config_path, CONFIG_FILE_NAME))
     twitter_config = config['twitter']
 
     auth = tweepy.OAuthHandler(twitter_config['consumer_key'], twitter_config['consumer_secret'])
     auth.set_access_token(twitter_config['access_token'], twitter_config['access_token_secret'])
     api = tweepy.API(auth)
 
-    public_tweets = api.user_timeline(include_rts=True)
+    public_tweets = api.user_timeline(count=50, include_rts=include_retweets)
 
-    for tweet in public_tweets[:10]:
+    for tweet in public_tweets:
         try:
             print(tweet.id, tweet.created_at, tweet.text)
         except AttributeError:
