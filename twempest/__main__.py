fda88a36c2c67b9cf372e21cfb64294ae2e62941
Dave Rogers <thedude@yukondude.com>
2017-02-13 08:30:50 -0800

    Ignore duplicate configuration file paths. Make sure configuration file is actually readable. Spit out an easier to diagnose configuration path error message. Remove redundant default argument to click option. Make peace in the Middle East.

--- twempest/__main__.py
+++ twempest/__main__.py
@@ -4,6 +4,7 @@
 # This file is part of Twempest. Copyright 2017 Dave Rogers <info@yukondude.com>. Licensed under the GNU General Public License, version 3.
 # Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
 
+import collections
 import configparser
 import os
 
@@ -12,21 +13,25 @@ import tweepy
 
 
 CONFIG_FILE_NAME = "twempest.conf"
-DEFAULT_CONFIG_PATH = "~/.twempest/"
+DEFAULT_CONFIG_PATH = "~/.twempest"
 
 
 def choose_config_path(cli_config_path):
     """ Choose the most likely configuration path from, in order: the given path, the default path, and the path of this module. To be
         valid, the config path must contain a twempest.conf file.
     """
-    here = os.path.dirname(os.path.realpath(__file__))
-    abs_cli_config_path = os.path.abspath(os.path.expanduser(cli_config_path))
+    # Using keys of OrderedDict simulates an OrderedSet type.
+    possible_paths = collections.OrderedDict([(os.path.abspath(os.path.expanduser(p)), None)
+                                              for p in (cli_config_path, DEFAULT_CONFIG_PATH, os.path.dirname(os.path.realpath(__file__)))])
 
-    for possible_path in (abs_cli_config_path, DEFAULT_CONFIG_PATH, here):
-        if os.path.isfile(os.path.join(possible_path, CONFIG_FILE_NAME)):
+    for possible_path in possible_paths:
+        possible_config_path = os.path.join(possible_path, CONFIG_FILE_NAME)
+
+        if os.path.isfile(possible_config_path) and os.access(possible_config_path, os.R_OK):
             return possible_path
 
-    raise click.ClickException("Could not find twempest.conf file in config path '{}' or alternate locations.".format(abs_cli_config_path))
+    raise click.ClickException("Could not find readable twempest.conf configuration file in path(s): '{}'"
+                               .format("', '".join(possible_paths.keys())))
 
 
 # noinspection PyUnusedLocal
@@ -44,8 +49,8 @@ def show_version(ctx, param, value):
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option("--config-path", "-c", default=DEFAULT_CONFIG_PATH, show_default=True,
               help="Twempest configuration directory path. The twempest.conf file must exist in this location.")
-@click.option("--replies", "-@", is_flag=True, default=False, help="Include @replies in the list of retrieved tweets.")
-@click.option("--retweets", "-r", is_flag=True, default=False, help="Include retweets in the list of retrieved tweets.")
+@click.option("--replies", "-@", is_flag=True, help="Include @replies in the list of retrieved tweets.")
+@click.option("--retweets", "-r", is_flag=True, help="Include retweets in the list of retrieved tweets.")
 @click.option("--version", "-V", is_flag=True, callback=show_version, expose_value=False, is_eager=True, help="Show version and exit.")
 def twempest(config_path, replies, retweets):
     """ Download a sequence of recent Twitter tweets and convert these, via template, to text format.
