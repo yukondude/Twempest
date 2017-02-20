a053865722b4acd0c7dec7a0b405a4fee9560433
Dave Rogers <thedude@yukondude.com>
2017-02-19 21:11:36 -0800

    Add slugify filter, largely borrowed from Django codebase.

--- twempest/filters.py
+++ twempest/filters.py
@@ -4,6 +4,9 @@
 # This file is part of Twempest. Copyright 2017 Dave Rogers <info@yukondude.com>. Licensed under the GNU General Public License, version 3.
 # Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
 
+import re
+import unicodedata
+
 import jinja2
 
 
@@ -23,3 +26,18 @@ def scrub(ctx, text):
         text = text.replace(url['url'], '')
 
     return text
+
+
+SLUG_1_RE = re.compile(r"[^\w\s-]")
+SLUG_2_RE = re.compile(r"[-\s]+")
+
+
+@jinja2.contextfilter
+def slugify(ctx, text):
+    """ Transform the given text into a suitable file name of no more than 80 characters that is also scrubbed of URLs and hashtags.
+    """
+    # TODO: properly acknowledge Django code
+    slug = scrub(ctx, text)
+    slug = unicodedata.normalize('NFKD', slug).encode('ascii', 'ignore').decode('ascii')
+    slug = SLUG_1_RE.sub('', slug).strip().lower()[:80]
+    return SLUG_2_RE.sub('-', slug)
