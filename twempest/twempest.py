fe9d14e61e15026af1a80263186786433b15ad10
Dave Rogers <thedude@yukondude.com>
2017-02-23 13:54:44 -0800

    Use local timezone instead of Twitter's UTC default.

--- twempest/twempest.py
+++ twempest/twempest.py
@@ -5,7 +5,9 @@
 # Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
 
 import jinja2
+import pytz
 import tweepy
+import tzlocal
 
 from twempest.filters import ALL_FILTERS
 
@@ -32,6 +34,9 @@ def render(auth_keys, options, template_text):
 
     api = authenticate_twitter_api(**auth_keys)
 
+    gmt_tz = pytz.timezone('UTC')
+    local_tz = tzlocal.get_localzone()
+
     try:
         tweets = list(tweepy.Cursor(api.user_timeline, since_id=options['since-id'], include_rts=options['retweets']).items())[-14:]
     except tweepy.TweepError as e:
@@ -39,6 +44,9 @@ def render(auth_keys, options, template_text):
                                 "See https://dev.twitter.com/overview/api/response-codes for an explanation.".format(e.response))
 
     for tweet in reversed(tweets):
+        # Replace UTC created time with local time.
+        tweet.created_at = gmt_tz.localize(tweet.created_at).astimezone(local_tz)
+
         if not options['replies'] and tweet.in_reply_to_status_id and tweet.text[0] == '@':
             continue
 
