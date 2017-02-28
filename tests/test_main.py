9e26301670d3be51da479cd7ccbf7d11fb43f4b3
Dave Rogers <thedude@yukondude.com>
2017-02-27 17:27:25 -0800

    Add unit test for choose_config_path().

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,12 +4,35 @@
 # This file is part of Twempest. Copyright 2017 Dave Rogers <info@yukondude.com>. Licensed under the GNU General Public License, version 3.
 # Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
 
+import os
 import re
 
 from click.testing import CliRunner
 
 from twempest import __version__
-from twempest.__main__ import twempest
+from twempest.__main__ import choose_config_path, CONFIG_FILE_NAME, twempest
+
+
+def test_choose_config_path():
+    # choose_config_path(cli_dir_path, default_dir_path, fallback_dir_path, file_name)
+    with CliRunner().isolated_filesystem():
+        os.makedirs("fff/ggg/hhh")
+        os.makedirs("iii/jjj")
+        os.makedirs("kkk")
+
+        root = os.getcwd()
+        os.chdir("fff/ggg")
+        possible_paths = [os.path.abspath(os.path.join(root, p)) for p in ["kkk", "iii/jjj", "fff/ggg"]]
+        print(possible_paths)
+
+        assert choose_config_path(possible_paths[0], possible_paths[1], possible_paths[2], CONFIG_FILE_NAME)[0] is None
+
+        for i in [2, 1, 0]:
+            with open(os.path.join(possible_paths[i], CONFIG_FILE_NAME), 'w') as f:
+                f.write("[twitter]")
+
+            assert (possible_paths[i], possible_paths) == choose_config_path(possible_paths[0], possible_paths[1], possible_paths[2],
+                                                                             CONFIG_FILE_NAME)
 
 
 HELP_OPTION_REGEX = re.compile(r"^Usage: twempest.+Show this message and exit\.$")
